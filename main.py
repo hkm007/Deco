9620761ca380d3127f8661810caee979c15ce6ba
hkm007 <hkm0072001@gmail.com>
2020-05-29 19:06:16 +0530

    directory issue fixed i think

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+from pathlib import Path
 
 def make_project(target, directory):
 	''' this function creates the whole project '''
 	# final project path
-	path = os.path.join(target, directory)
+	# path = os.path.join(target, directory)
+	path = Path(target, directory)
 
 	try:
 		os.mkdir(path)
@@ -11,38 +13,40 @@ def make_project(target, directory):
 		print(e)
 
 	# creates index.html
-	index = path+"/index.html"
+	index = path / "index.html"
 
 	f = open(index, "w")
 	f.write("Welcome to Deno")
 	f.close()
 
+
 	# creates a folder css and make file style.css in it
-	css_path = path+"/css"
+	css_path = path / "css"
 	try:
 		os.mkdir(css_path)
 	except Exception as e:
 		print(e)
 
-	style = css_path+"/style.css"
+	style = css_path / "style.css"
 
 	f = open(style, "w")
 	f.close()
 
 	# creates a folder js and make file main.js in it
-	js_path = path+"/js"
+	js_path = path / "js"
 	try:
 		os.mkdir(js_path)
 	except Exception as e:
 		print(e)
 
-	javascript = js_path+"/main.js"
+	javascript = js_path / " main.js"
 
 	f = open(javascript, "w")
+	f.write("console.log('working!')")
 	f.close()
 
 	# creates assets folder for images etc
-	assets_path = path+"/assets"
+	assets_path = path / "assets"
 	try:
 		os.mkdir(assets_path)
 	except Exception as e:
@@ -55,4 +59,4 @@ if __name__ == '__main__':
 
 	make_project(target, directory)
 
-	print("success")
+	print("Project successfully created!")
