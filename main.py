aacbc35823abbefbb2dd2855addfbce79c863dad
Deepraj <bdeeprajrkm1@gmail.com>
2020-05-29 23:48:36 +0530

    fixed some bugs

--- main.py
+++ main.py
@@ -12,85 +12,52 @@ def make_project(target,directory):
 		create_css(path)
 		create_js(path)
 		create_assets(path)
-	except:
-		print("Project cannot be created.")
-		print("A directory with same name already exists!")
-		exit()
-
+	except Exception as e:
+		print(e)
 
 
 def create_html(path):
 	# creates index.html
 	index_path = path / "index.html"
-	try:
-		# os.mkdir(index_path)
-		f = open(index_path, "w")
-		f.write("""<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		<!-- local css -->
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-
-		<title>Deco</title>
-	</head>
-	<body>
-		<center><p><b>Welcome to Deco</b></p></center><br><br>
-
-		<center>
-			<img src="https://cdnb.artstation.com/p/assets/images/images/005/731/019/original/david-bautista-fire2.gif?1493342838" alt="landing_img">
-		</center>	
-
-		<!-- local javascript -->
-		<script src="js/main.js"></script>
-	</body>
-	</html>""")
-		f.close()
-
-	except Exception as e:
-		print(e)
-
+	f = open(index_path, "w")
+	f.write("""<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+	<h2>Hello World </h2>
+</body>
+</html>""")
+	f.close()
 
 def create_css(path):
 	# creates a folder css and make file style.css in it
 	css_path = path / "css"
 	try:
 		os.mkdir(css_path)
-		style = css_path / "style.css"
-
-		f = open(style, "w")
-		f.write("""/* write your css here */
-	body {
-		background-color: #000000;
-	}
-
-	p {
-		color: white;
-		font-size: 50px;
-	}
-
-	img {
-		height: 300px;
-		width: 300px;
-	}""")
-		f.close()
 	except Exception as e:
 		print(e)
 
+	style = css_path / "style.css"
+
+	f = open(style, "w")
+	f.close()
 
 def create_js(path):
 	# creates a folder js and make file main.js in it
 	js_path = path / "js"
 	try:
 		os.mkdir(js_path)
-		javascript = js_path / "main.js"
-		f = open(javascript, "w")
-		f.write("console.log('working!')")
-		f.close()
 	except Exception as e:
 		print(e)
+
+	javascript = js_path / "main.js"
+
+	f = open(javascript, "w")
+	f.close()
 
 def create_assets(path):
 	# creates assets folder for images etc
@@ -103,9 +70,10 @@ def create_assets(path):
 if __name__ == '__main__':
 	# construct the argument parse and parse the arguments
 	parser = argparse.ArgumentParser(description= "Create Boiler plate for HTML,CSS,JS")
-	parser.add_argument("--startproject" ,type = str,help="Boiler plate project name")
+	parser.add_argument("--startproject" ,type = str,help="Boiler plate name")
 	args = vars(parser.parse_args())
 	dir_name = args['startproject']
 	target = './'
 	make_project(target,dir_name)
-	print("Project successfully created as '{}'!".format(dir_name))
+	print("Created boiler plate at {}!!".format(args['startproject']))
+
